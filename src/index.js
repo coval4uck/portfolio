0022c38ffac83c8a7f8b43450ed9a2fa46d2dbfe
coval4uck <coval4uck.valiik@gmail.com>
2024-12-23 22:54:51 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { ThemeProvider } from './provider/ThemeProvider';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-      <BrowserRouter>
+      <BrowserRouter basename='/'>
         <ThemeProvider>
           <Portfolio />
         </ThemeProvider>
